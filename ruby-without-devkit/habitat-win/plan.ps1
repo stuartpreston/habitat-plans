6acf1566a848ff866174398eba5771a4601af51d
Stuart Preston <stuart@chef.io>
2019-05-01 23:46:29 +0100

    Updating to Ruby 2.6.3
    Signed-off-by: Stuart Preston <stuart@chef.io>

--- ruby-without-devkit/habitat-win/plan.ps1
+++ ruby-without-devkit/habitat-win/plan.ps1
@@ -1,20 +1,15 @@
-$pkg_name="ruby-plus-devkit"
+$pkg_name="ruby-without-devkit"
 $pkg_origin="stuartpreston"
 $pkg_version="2.6.3"
 $pkg_revision="1"
 $pkg_maintainer="Stuart Preston <stuart@chef.io>"
 $pkg_license=@("Apache-2.0")
-$pkg_source="https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-${pkg_version}-${pkg_revision}/rubyinstaller-devkit-${pkg_version}-${pkg_revision}-x64.exe"
-$pkg_shasum="d30a9d765cc10c744bc5b032d407c763fcbee0c86ee4d6c83dc669318f13e1bb"
+$pkg_source="https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-${pkg_version}-${pkg_revision}/rubyinstaller-${pkg_version}-${pkg_revision}-x64.exe"
+$pkg_shasum="8223b330ca7d100c7f07e881985299768a1818cb8572a477910c6c13bea55947"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Unpack {
    Start-Process "$HAB_CACHE_SRC_PATH/$pkg_filename" -Wait -ArgumentList "/SP- /NORESTART /VERYSILENT /SUPPRESSMSGBOXES /NOPATH /DIR=$HAB_CACHE_SRC_PATH/$pkg_dirname"
-}
-
-function Invoke-Build {
-   # Launch msys2 once in order to initialize the environment
-   Invoke-Expression -Command "cmd /c $HAB_CACHE_SRC_PATH/$pkg_dirname/msys64\msys2_shell.cmd exit /b 0" -Verbose
 }
 
 function Invoke-Install {
